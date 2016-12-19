74846e17d1947ab5b7c96977168acc5a61297b25
Perry <N/A>
2016-12-19 13:21:46 -0600

    First commit.

---
+++ Decimal2Radix/Decimal2Radix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal2Radix
+{
+    public class Decimal2Radix
+    {
+        public static String RadixConvert(int decNum, int radix)
+        {
+            String radNum = "";
+            int digit = 0;
+            char next = ' ';
+
+            while (decNum > 0)
+            {
+                digit = decNum % radix;
+                decNum = decNum / radix;
+
+                if (digit < 10)
+                {
+                    next = (char)(digit + 48);
+                }
+                else if (digit < 36)
+                {
+                    next = (char)(digit + 55);
+                }
+
+                radNum = next + radNum;
+            }
+            
+            return radNum;
+        }
+    }
+}
---
+++ Decimal2RadixConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal2RadixConsole
+{
+    using Decimal2Radix;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decNum = 0;
+            int radix = 0;
+
+        Start:
+            Console.Write("Enter a decimal number: ");
+            decNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a radix: ");
+            radix = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(decNum + " (base 10) = " + Decimal2Radix.RadixConvert(decNum, radix) + " (base " + radix + ")");
+
+            goto Start;
+        }
+    }
+}
